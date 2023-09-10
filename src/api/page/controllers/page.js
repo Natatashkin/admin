56ddf71a6960de6555626081207063051e237714
Natatashkin <cs070283sng@gmail.com>
2023-09-10 23:29:20 +0300

    add tsconfig

--- src/api/page/controllers/page.js
+++ src/api/page/controllers/page.js
@@ -1,9 +1,23 @@
-'use strict';
+"use strict";
 
 /**
  * page controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::page.page');
+module.exports = createCoreController("api::page.page", ({ strapi }) => ({
+  async findOne(ctx) {
+    const { id } = ctx.params;
+
+    console.log(id);
+    const entity = await strapi.db.query("api::page.page").findOne({
+      where: { slug: id },
+    });
+
+    // strapi.log.debug(entity.customSlug);
+    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+    return this.transformResponse(sanitizedEntity);
+  },
+}));
