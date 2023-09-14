04660c3d5b21264f7b022c68dfe03e5007ee07bf
Natatashkin <cs070283sng@gmail.com>
2023-09-14 17:26:03 +0300

    add strapi-plugin-populate-deep, plugins.js

--- src/api/page/controllers/page.js
+++ src/api/page/controllers/page.js
@@ -9,13 +9,21 @@ const { createCoreController } = require("@strapi/strapi").factories;
 module.exports = createCoreController("api::page.page", ({ strapi }) => ({
   async findOne(ctx) {
     const { id } = ctx.params;
-
-    console.log(id);
     const entity = await strapi.db.query("api::page.page").findOne({
       where: { slug: id },
+      populate: {
+        blocks: {
+          on: {
+            "elements.features": {
+              features: {
+                icon: true
+              }
+            }
+          }
+        }
+      }
     });
 
-    // strapi.log.debug(entity.customSlug);
     const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
 
     return this.transformResponse(sanitizedEntity);
